52215f4847e0de51d171d8b56ec2a5d20ec9c027
Kai Schell <kai.schell@oscert.eu>
2023-08-07 15:31:41 +0200

    add function

--- user_input_converter.py
+++ user_input_converter.py
@@ -1,17 +1,12 @@
 # TODO: 
 # - Möglichkeit, nach falscher Eingabe neue Eingabe zu tätigen    
 # - evtl. Problem falls Float eingegeben wird. Prüfung möglich?
+# - Code besser strukturieren/aufräumen
 
-
-#anzahl_tage = input("Bitte gib die Anzahl der Tage an, die konvertiert werden sollen.")    # gibt einen String zurück
-#print(type(anzahl_tage))
-
-# explizites Casting von str nach int
-#anzahl_tage_int = int(anzahl_tage)
-#print(type(anzahl_tage_int))
+def umrechnung_tage_in_stunden():
+    print(str(anzahl_tage) + " Tage sind " + str(anzahl_tage * umrechung_stunden) + " Stunden.")
 
 anzahl_tage = input("Bitte gib die Anzahl der Tage an, die konvertiert werden sollen.\n")
-#print(type(anzahl_tage))
 
 umrechung_stunden = 24
 umrechung_minuten = 24 * 60
@@ -22,7 +17,7 @@ if anzahl_tage.isdigit():
     anzahl_tage = int(anzahl_tage)
 
     if anzahl_tage > 0:
-        print(str(anzahl_tage) + " Tage sind " + str(anzahl_tage * umrechung_stunden) + " Stunden.")
+        umrechnung_tage_in_stunden()
     elif anzahl_tage == 0:
         print("Die eingegebene Zahl ist gleich 0, das Ergebnis kennst du selbst...")
     else:
@@ -30,14 +25,3 @@ if anzahl_tage.isdigit():
 
 else:
     print("Bitte nur ganze Zahlen eingeben und keinen Quatsch!")
-
-#print(str(anzahl_tage) + " Tage sind " + str(anzahl_tage * umrechung_minuten) + " Minuten.")
-#print(str(anzahl_tage) + " Tage sind " + str(anzahl_tage * umrechung_sekunden) + " Sekunden.")
-
-# Vergleichsoperatoren:
-# >
-# <
-# >=
-# <=    
-# ==
-
