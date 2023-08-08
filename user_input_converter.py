fdc5de6d0aaeae00290e96ef973eac1902ffb76c
Kai Schell <kai.schell@oscert.eu>
2023-08-08 13:47:11 +0200

    while true loop

--- user_input_converter.py
+++ user_input_converter.py
@@ -6,22 +6,29 @@
 def umrechnung_tage_in_stunden():
     print(str(anzahl_tage) + " Tage sind " + str(anzahl_tage * umrechung_stunden) + " Stunden.")
 
-anzahl_tage = input("Bitte gib die Anzahl der Tage an, die konvertiert werden sollen.\n")
-
 umrechung_stunden = 24
 umrechung_minuten = 24 * 60
 umrechung_sekunden = 24 * 60 * 60
 
-if anzahl_tage.isdigit():
+# while - Start
+while True:      # bewusst erstellte Endlosschleife
+    anzahl_tage = input("Bitte gib die Anzahl der Tage an, die konvertiert werden sollen.\n")
 
-    anzahl_tage = int(anzahl_tage)
+    if anzahl_tage.isdigit():
 
-    if anzahl_tage > 0:
-        umrechnung_tage_in_stunden()
-    elif anzahl_tage == 0:
-        print("Die eingegebene Zahl ist gleich 0, das Ergebnis kennst du selbst...")
+        anzahl_tage = int(anzahl_tage)
+
+        if anzahl_tage > 0:
+            umrechnung_tage_in_stunden()
+            break       # an dieser Stelle wird die Schleife verlassen
+        elif anzahl_tage == 0:
+            print("Die eingegebene Zahl ist gleich 0, das Ergebnis kennst du selbst...")
+        else:
+            print("Die eingegebene Zahl ist kleiner als 0, Konvertierung nicht möglich.")
+
     else:
-        print("Die eingegebene Zahl ist kleiner als 0, Konvertierung nicht möglich.")
+        print("Bitte nur ganze Zahlen eingeben und keinen Quatsch!")
 
-else:
-    print("Bitte nur ganze Zahlen eingeben und keinen Quatsch!")
+# while - Ende
+
+print("Programm beendet")
