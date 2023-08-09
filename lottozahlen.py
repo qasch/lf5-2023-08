40f7445d64d16514eb238058164d226c2923809f
Kai Schell <kai.schell@oscert.eu>
2023-08-09 14:35:17 +0200

    first steps

--- lottozahlen.py
+++ lottozahlen.py
@@ -1,3 +1,5 @@
+import random
+
 # Basisfunktionalität:
 # - Nutzer soll aufgefordert werden, sechs Zahlen zwischen 1 und 49 einzugeben
 # - Wir generieren zusätzlich sechs Zufallszahlen
@@ -13,7 +15,29 @@
 # - Eingabe wird in einer List gespeichert 
 # - Zu Beginn auf Eingabemöglichkeit verzichten, erst Logik implementieren, erst dann Eingabe und Validierung
 # - Zufallszahlen mit dem Modul random generieren (muss importiert werden)
+# - bei den generierten Zahlen darauf achten, dass es keine Dopplungen gibt
 # - auch Zufallszahlen in Liste speichern
 # - Zufallszahlen ausgeben
 # - Vergleich der Eingabe mit den Zufallszahlen (wie genau machen wir das?)
 # - Abschlussausgabe
+
+# TODO: später über input() Benutzer nach den Zahlen fragen
+tipp = [8, 23, 17, 34, 3, 9]
+
+zufallszahlen = []
+# Nutzung der anonymen Variable _, 
+# da wir dies nur für die Definition brauchen, 
+# aber nicht in unserem Code nutzen
+for _ in range(6):       
+    zufallszahlen.append(random.randint(1, 49))
+
+print("Zufallszahlen:", zufallszahlen)
+
+
+
+
+
+
+
+
+
